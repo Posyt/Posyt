f3f0fa452575c7fc5ea2315015a86771269d0d13
Chris Smothers <chris.smothers@gmail.com>
2016-12-08 00:42:44 -0500

    ddp pre2

--- src/lib/DDP.js
+++ src/lib/DDP.js
@@ -264,7 +264,7 @@ class DDP {
       autoReconnect: true,
       autoReconnectTimer: 500,
       maintainCollections: true,
-      ddpVersion: '1',  // ['1', 'pre2', 'pre1'] available
+      ddpVersion: 'pre2',  // ['1', 'pre2', 'pre1'] available
       // Use a full url instead of a set of `host`, `port` and `ssl`
       // url: 'wss://example.com/websocket'
       // socketConstructor: WebSocket // Another constructor to create new WebSockets
