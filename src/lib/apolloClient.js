30ffe33257e5c15728a3f881804f5666df799ebc
Chris Smothers <chris.smothers@gmail.com>
2017-02-13 00:07:52 -0500

    playing with loading auth token

--- src/lib/apolloClient.js
+++ src/lib/apolloClient.js
@@ -1,3 +1,25 @@
-import ApolloClient from 'apollo-client';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import {
+  AsyncStorage,
+} from 'react-native';
+import {
+  posytGraphQLUri,
+} from './constants';
 
-export const apolloClient = new ApolloClient();
+const networkInterface = createNetworkInterface({
+  uri: posytGraphQLUri,
+});
+
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) req.options.headers = {};
+    // console.log(global.authToken)
+    const authToken = '52KIpAUrC-ZUQhY6iellioVTq37FTffZ4_LMGu705SY';
+    if (authToken) req.options.headers.authorization = authToken;
+    next();
+  }
+}]);
+
+export const apolloClient = new ApolloClient({
+  networkInterface,
+});
